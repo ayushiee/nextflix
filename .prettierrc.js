abf4c31873b7fe6c170af2f6260c5b55602aa118
Ayushi <ayushigupta8@gmail.com>
2021-07-23 14:28:11 +0530

    feat: added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 2,
+    trailingComma: 'none',
+    jsxBracketSameLine: true,
+    jsxSingleQuote: true,
+    arrowParens: 'avoid'
+};
