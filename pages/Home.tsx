49913c3aa26e10eb767b04ab56bc1dee06d42355
Ayushi <ayushigupta8@gmail.com>
2021-07-26 01:13:28 +0530

    chore: added initial navbar styles

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
+const listLeft = ['Home', 'TV Shows', 'Movies', 'New & Popular', 'My List'];
+const listRight = ['icon1', 'icon2', 'icon3', 'icon4'];
 
 export default function Home() {
   return (
@@ -11,16 +14,37 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
+      <div className={styles.navBar}>
+        <div className={styles.navBar__left}>
+          {listLeft.map((item, index) => (
+            <div key={index} className={styles.options}>
+              {item}
+            </div>
+          ))}
+        </div>
+        <div className={styles.navBar__right}>
+          {listRight.map((item, index) => (
+            <div key={index} className={styles.options}>
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+
       <div className={styles.spotlight}>
-        <Image
+        <img
           src='https://images.unsplash.com/photo-1460881680858-30d872d5b530?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80'
           alt='spotlight'
-          height={400}
-          width={1200}
+          // height={400}
+          // width={1200}
           className={styles.spotlight__image}
         />
       </div>
-      <div className={styles.bottomContainer}>bottom</div>
+
+      <div className={styles.bottomContainer}>
+        bottom
+        <div>hehhehehe</div>
+      </div>
     </div>
   );
 }
