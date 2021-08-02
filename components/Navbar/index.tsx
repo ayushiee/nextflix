6e7b0e5ff2d758c7dfcafb981db925ab5166acbd
Ayushi <ayushigupta8@gmail.com>
2021-08-03 00:24:42 +0530

    chore: added search

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState, useRef } from 'react';
+import { useState, useRef, ChangeEvent } from 'react';
 import Image from 'next/image';
 import { NextRouter, useRouter } from 'next/router';
 import { motion } from 'framer-motion';
@@ -10,9 +10,9 @@ import styles from '../../styles/Navbar.module.scss';
 import nfLogo from '../../public/assets/nfLogo.png';
 import Dialog from '../Dialog';
 import { ROUTES } from '../../utils/config';
-import SearchBar from './SearchBar';
 import { Maybe } from '../../utils/types';
 import useExternalClick from '../../hooks/useExternalClick';
+import Profile from './Profile';
 
 const listLeft = ['Home', 'TV Shows', 'Movies', 'New & Popular', 'My List'];
 
@@ -69,41 +69,34 @@ export default function Navbar({ isScrolled }: NavbarProps): React.ReactElement 
 
       <div className={styles.navBar__right}>
         <div ref={searchRef} className={styles.searchPanel}>
-        <motion.div
-          className={styles.searchBar}
-          initial='hidden'
-          animate={isSearch ? 'visible' : 'hidden'}
-          transition={{ duration: 0.5 }}
-          variants={{
-            visible: {
-              opacity: 1,
-              width: 250
-            },
-            hidden: {
-              opacity: 0,
-              width: 0
-            }
-          }}>
-          <IoSearch className={styles.icon} />
-          <input
-            type='text'
-            className={styles.searchBar__input}
-            value={searchInput}
-            onChange={onSearchQuery}
-            placeholder='Titles, people, genres'
-          />
-        </motion.div>
-        {!isSearch && <IoSearch className={styles.icon} onMouseOver={onSearchActive} />}
-      </div>
-        <IoNotifications className={styles.icon} />
-
-        <div className={styles.profile} onMouseOver={onHover}>
-          <img src='../assets/avatar.png' alt='user' className={styles.user} />
-          <BiCaretDown className={styles.smallIcon} />
-          <Dialog dialogRef={profileRef} onClose={onClose} classname={styles.signout} visible={visible}>
-            <div onClick={onSignout}>Sign out</div>
-          </Dialog>
+          <motion.div
+            className={styles.searchBar}
+            initial='hidden'
+            animate={isSearch ? 'visible' : 'hidden'}
+            transition={{ duration: 0.5 }}
+            variants={{
+              visible: {
+                opacity: 1,
+                width: 250
+              },
+              hidden: {
+                opacity: 0,
+                width: 0
+              }
+            }}>
+            <IoSearch className={styles.icon} />
+            <input
+              type='text'
+              className={styles.searchBar__input}
+              value={searchInput}
+              onChange={onSearchQuery}
+              placeholder='Titles, people, genres'
+            />
+          </motion.div>
+          {!isSearch && <IoSearch className={styles.icon} onMouseOver={onSearchActive} />}
         </div>
+        <IoNotifications className={styles.icon} />
+        <Profile />
       </div>
     </motion.div>
   );
