b74502da30844a47092540ea58f377e27ff66b9e
Ayushi <ayushigupta8@gmail.com>
2021-08-03 13:56:06 +0530

    chore: added breakpoint types

--- utils/config.ts
+++ utils/config.ts
@@ -1,5 +1,23 @@
 export enum ROUTES {
-    HOME = '/',
-    BROWSE = '/browse',
-    LATEST = '/latest'
+  HOME = '/',
+  BROWSE = '/browse',
+  LATEST = '/latest'
+}
+
+export const BreakpointValues = {
+  XS: 480,
+  SM: 640,
+  MD: 768,
+  LG: 1025,
+  XL: 1280,
+  XXL: 1536
+};
+
+export enum Breakpoint {
+  XS = 'XS',
+  SM = 'SM',
+  MD = 'MD',
+  LG = 'LG',
+  XL = 'XL',
+  XXL = 'XXL'
 }
--- utils/types.ts
+++ utils/types.ts
@@ -1,1 +1,16 @@
+import { Breakpoint } from './config';
+
 export type Maybe<T> = T | null;
+
+export type Dimension = {
+  height: number;
+  width: number;
+};
+
+export type DimensionDetail = {
+  dimension: Dimension;
+  breakpoint: Breakpoint;
+  isMobile: boolean;
+  isTablet: boolean;
+  isDesktop: boolean;
+};
