15a5191500a38e98fda73c834f2f1ec98447739e
Ayushi <ayushigupta8@gmail.com>
2021-08-09 14:19:49 +0530

    feat: added default list hover

--- components/List/Cards.tsx
+++ components/List/Cards.tsx
@@ -1,13 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from '../../styles/Cards.module.scss';
 
 interface CardsProps {
   defaultCard?: boolean;
 }
 
+const DUMMY_IMAGE: string = 'https://source.unsplash.com/random';
+
 export default function Cards({ defaultCard = true }: CardsProps): React.ReactElement {
+  const height = defaultCard ? '9rem' : '9rem';
   return (
-    <div className={styles.cardRowss}>
-      {defaultCard ? <div className={styles.card}></div> : <div className={styles.longCard}></div>}
-    </div>
+    <>
+      <div className={styles.card} style={{ height: `${height}` }}>
+        <img src={DUMMY_IMAGE} alt='img' className={styles.cardPoster} />
+        <div className={styles.cardInfo}>bottom</div>
+      </div>
+    </>
   );
 }
