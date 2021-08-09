a755abaa2049e2388b9a39591242dd4c533d2455
Ayushi <ayushigupta8@gmail.com>
2021-08-09 12:51:35 +0530

    fix: minor

--- components/List/index.tsx
+++ components/List/index.tsx
@@ -9,15 +9,13 @@ interface ListProps {
 
 export default function List({ defaultCard = true, arr, title }: ListProps): React.ReactElement {
   return (
-    <>
-      <div className={styles.listContainer}>
-        <strong className={styles.category}>{title}</strong>
-        <div className={styles.cardRow}>
-          {arr.map((_, index) => {
-            return <Cards key={index} defaultCard={defaultCard} />;
-          })}
-        </div>
+    <div className={styles.listContainer}>
+      <strong className={styles.category}>{title}</strong>
+      <div className={styles.cardRow}>
+        {arr.map((_, index) => {
+          return <Cards key={index} defaultCard={defaultCard} />;
+        })}
       </div>
-    </>
+    </div>
   );
 }
