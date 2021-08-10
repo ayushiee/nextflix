6539bf338f5372693ab537fc50e9fe0d212a97e3
Ayushi <ayushigupta8@gmail.com>
2021-08-10 22:47:16 +0530

    chore: updated button styles

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,28 +1,30 @@
 import styles from '../styles/Button.module.scss';
 import { IconType } from 'react-icons';
+import { useState } from 'react';
 
 interface ButtonProps {
   filled?: boolean;
   label?: string;
   Icon: IconType;
-  filledRounded?: boolean;
-  outlineRounded?: boolean;
+  rounded?: boolean;
 }
 
 export default function Button(props: ButtonProps): React.ReactElement {
-  const { filled, label, Icon, filledRounded, outlineRounded } = props;
-  const backgroundColor = filled || filledRounded ? 'white' : '#6d6d6db3';
-  const fontColor = filled || filledRounded ? 'black' : 'white';
+  const { filled, label, Icon, rounded } = props;
+  const backgroundColor = filled ? 'white' : '#6d6d6db3';
+  const fontColor = filled ? 'black' : 'white';
+  
+  /* 
+  if not rounded === normal long style
+  if filled ( and rounded) === round style
+  if rounded and not filled === outline style
+  */
+  const style = !rounded ? styles.button : filled ? styles.roundButton : styles.outlineRounded;
 
   return (
-    <>
-       <button className={styles.button} style={{ backgroundColor: `${backgroundColor}`, color: `${fontColor}` }}>
-        <Icon className={styles.icon} />
-        <span className={styles.label}>{label}</span>
-      </button>
-       {outlineRounded && <button className={styles.outlineRounded} style={{ backgroundColor: `${backgroundColor}`, color: `${fontColor}` }}>
-        <Icon className={styles.icon} />
-      </button>}
-    </>
+    <button className={style} style={{ backgroundColor: `${backgroundColor}`, color: `${fontColor}` }}>
+      <Icon className={styles.icon} />
+      {!rounded && <span className={styles.label}>{label}</span>}
+    </button>
   );
 }
