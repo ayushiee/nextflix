590fc29f1f8b3096eec6d5476413b5416f0f1efa
Ayushi <ayushigupta8@gmail.com>
2021-08-10 23:13:54 +0530

    chore: added long card details

--- components/List/Cards.tsx
+++ components/List/Cards.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from '../../styles/Cards.module.scss';
+import { Add, Play, Down } from '../../utils/icons';
+import Button from '../Button';
 
 interface CardsProps {
   defaultCard?: boolean;
@@ -10,17 +12,34 @@ const DUMMY_IMAGE: string = 'https://source.unsplash.com/random';
 export default function Cards({ defaultCard = true }: CardsProps): React.ReactElement {
   const style = defaultCard ? styles.card : styles.longCard;
   return (
-      <div className={style}>
-        <img src={DUMMY_IMAGE} alt='img' className={styles.cardPoster} />
-        {defaultCard ? (
-          <div className={styles.cardInfo}>bottom</div>
-        ) : (
-            <div className={styles.more}>
-              More
-              <strong>Title</strong>
-              <span>New</span>
+    <div className={style}>
+      <img src={DUMMY_IMAGE} alt='img' className={styles.cardPoster} />
+      {defaultCard ? (
+        <div className={styles.cardInfo}>bottom</div>
+      ) : (
+        <div className={styles.more}>
+          <div className={styles.actionRow}>
+            <div className={styles.actionRow}>
+              <Button Icon={Play} rounded filled />
+              <Button Icon={Add} rounded />
             </div>
-        )}
-      </div>
+            <Button Icon={Down} rounded />
+          </div>
+          <div className={styles.textDetails}>
+            <strong>Title</strong>
+            <div className={styles.row}>
+              <span className={styles.greenText}>New</span>
+              <span className={styles.rating}>rating</span>
+              <span className={styles.regularText}>length </span>
+            </div>
+            <div className={styles.row}>
+              <span className={styles.regularText}>length </span>
+              <span className={styles.regularText}>length </span>
+              <span className={styles.regularText}>length </span>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
