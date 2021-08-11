ac3637dbc706c289b57e7b7322a71bebc765d6ac
Ayushi <ayushigupta8@gmail.com>
2021-08-12 01:03:11 +0530

    chore: added default card info

--- components/List/Cards.tsx
+++ components/List/Cards.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from '../../styles/Cards.module.scss';
-import { Add, Play, Down } from '../../utils/icons';
+import { Add, Play, Down, Like, Dislike } from '../../utils/icons';
 import Button from '../Button';
 
 interface CardsProps {
@@ -11,11 +11,65 @@ const DUMMY_IMAGE: string = 'https://source.unsplash.com/random';
 
 export default function Cards({ defaultCard = true }: CardsProps): React.ReactElement {
   const style = defaultCard ? styles.card : styles.longCard;
+  const infoStyle = defaultCard ? styles.cardInfo : styles.more;
+
   return (
     <div className={style}>
       <img src={DUMMY_IMAGE} alt='img' className={styles.cardPoster} />
-      {defaultCard ? (
-        <div className={styles.cardInfo}>bottom</div>
+      <div className={infoStyle}>
+        <div className={styles.actionRow}>
+          <div className={styles.actionRow}>
+            <Button Icon={Play} rounded filled />
+            <Button Icon={Add} rounded />
+            {defaultCard && (
+              <>
+                <Button Icon={Like} rounded />
+                <Button Icon={Dislike} rounded />
+              </>
+            )}
+          </div>
+          <Button Icon={Down} rounded />
+        </div>
+        <div className={styles.textDetails}>
+          <strong>Title</strong>
+          <div className={styles.row}>
+            <span className={styles.greenText}>New</span>
+            <span className={styles.rating}>rating</span>
+            <span className={styles.regularText}>length </span>
+          </div>
+          <div className={styles.row}>
+            <span className={styles.regularText}>length </span>
+            <span className={styles.regularText}>length </span>
+            <span className={styles.regularText}>length </span>
+          </div>
+        </div>
+      </div>
+
+      {/* {defaultCard ? (
+        <div className={styles.cardInfo}>
+          <div className={styles.actionRow}>
+            <div className={styles.actionRow}>
+              <Button Icon={Play} rounded filled />
+              <Button Icon={Add} rounded />
+              <Button Icon={Like} rounded />
+              <Button Icon={Dislike} rounded />
+            </div>
+            <Button Icon={Down} rounded />
+          </div>
+          <div className={styles.textDetails}>
+            <strong>Title</strong>
+            <div className={styles.row}>
+              <span className={styles.greenText}>New</span>
+              <span className={styles.rating}>rating</span>
+              <span className={styles.regularText}>length </span>
+            </div>
+            <div className={styles.row}>
+              <span className={styles.regularText}>length </span>
+              <span className={styles.regularText}>length </span>
+              <span className={styles.regularText}>length </span>
+            </div>
+          </div>
+        </div>
       ) : (
         <div className={styles.more}>
           <div className={styles.actionRow}>
@@ -39,7 +93,7 @@ export default function Cards({ defaultCard = true }: CardsProps): React.ReactEl
             </div>
           </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
