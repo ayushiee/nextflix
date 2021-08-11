191e79ca0c7d9b634fdb85637c1ab2e2752ca9ec
Ayushi <ayushigupta8@gmail.com>
2021-08-11 23:17:13 +0530

    chore: new icons

--- utils/icons.ts
+++ utils/icons.ts
@@ -1,5 +1,6 @@
 import { BiInfoCircle as Info, BiCaretDown as CaretDown } from 'react-icons/bi';
 import { IoPlaySharp as Play, IoSearch as Search, IoNotifications as Notifications, IoAddSharp as Add, IoChevronDown as Down } from 'react-icons/io5';
+import { AiOutlineLike as Like, AiOutlineDislike as Dislike } from 'react-icons/ai';
 
-export { Info, CaretDown, Play, Search, Notifications, Add, Down };
+export { Info, CaretDown, Play, Search, Notifications, Add, Down, Like, Dislike };
 
