cfb06fe5fdd92b647343870c4a1372bb80ed5c74
Ayushi <ayushigupta8@gmail.com>
2021-08-18 18:07:29 +0530

    chore: footer icons

--- utils/icons.ts
+++ utils/icons.ts
@@ -1,6 +1,14 @@
 import { BiInfoCircle as Info, BiCaretDown as CaretDown } from 'react-icons/bi';
-import { IoPlaySharp as Play, IoSearch as Search, IoNotifications as Notifications, IoAddSharp as Add, IoChevronDown as Down } from 'react-icons/io5';
+import {
+  IoPlaySharp as Play,
+  IoSearch as Search,
+  IoNotifications as Notifications,
+  IoAddSharp as Add,
+  IoChevronDown as Down,
+  IoLogoLinkedin as Linkedin,
+  IoLogoTwitter as Twitter,
+  IoLogoGithub as Github
+} from 'react-icons/io5';
 import { AiOutlineLike as Like, AiOutlineDislike as Dislike } from 'react-icons/ai';
 
-export { Info, CaretDown, Play, Search, Notifications, Add, Down, Like, Dislike };
-
+export { Info, CaretDown, Play, Search, Notifications, Add, Down, Like, Dislike, Linkedin, Twitter, Github };
