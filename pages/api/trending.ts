8ef1f8bd30258041c556a9230c92153c39624f58
Ayushi <ayushigupta8@gmail.com>
2021-08-21 01:37:25 +0530

    feat: added trending api

---
+++ pages/api/trending.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from '../../utils/axios';
+import { Media, MediaType } from '../../types';
+import { parse } from '../../utils/apiResolvers';
+
+interface Response {
+  type: 'Success' | 'Error';
+  data: Media[] | Error;
+}
+
+const apiKey = process.env.TMDB_KEY;
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse<Response>) {
+  const { type } = request.query;
+
+  try {
+    const result = await axios().get(`/trending/${type}/day`, {
+      params: {
+        api_key: apiKey,
+        watch_region: 'US'
+      }
+    });
+    const data = parse(result.data.results, type as MediaType);
+
+    response.status(200).json({ type: 'Success', data });
+  } catch (error) {
+    console.log(error.data);
+    response.status(500).json({ type: 'Error', data: error.data });
+  }
+}
