2e8f948af6be9ae4894b8cc8c42fa9e2472e899f
Ayushi <ayushigupta8@gmail.com>
2021-08-31 14:56:50 -0700

    chore: removed component index

--- components/index.tsx
+++
@@ -1,9 +0,0 @@
-import Button from './Button';
-import Banner from './Banner';
-import Footer from './Footer';
-import Navbar from './Navbar';
-import Dialog from './Dialog';
-import List from './List';
-import Layout from './Layout';
-
-export { Button, Banner, Footer, Navbar, Dialog, List, Layout };
